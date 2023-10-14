8a23246c7ab26ee272193bb409634bb1bd541b58
Tetiana Kobzar <140975446+TKobzar777@users.noreply.github.com>
2023-10-15 00:26:00 +0100

    Update hw_09.py
    improved

--- hw_09.py
+++ hw_09.py
@@ -47,49 +47,51 @@ def helf_cla():
         '\tPHONE (name) - phone search by name\n'\
         '\tCHANGE (name and new phone number separated by space) - stores the new phone number of the existing contact\n'\
         '\tSHOW ALL -displays all saved contacts with phone numbers in the console\n'\
-        '\tGOOD BAY, CLOSE, EXIT - shutdown\n'\
+        '\tGOOD BYE, CLOSE, EXIT - shutdown\n'\
         '\tHELP - Displays this alarm in the console\n'\
         'The bot is not sensitive to the register of commands entered\n'\
         'Success in your work!'
         
-    
+def command_parser(stp_user_input)->str:
+    list_user_input=stp_user_input.split()
+        
+    if list_user_input[0].lower() == 'hello':
+        return 'How can I help you?'
+        
+    if list_user_input[0].lower() == 'add':
+        return add_contact(*list_user_input)
+        
+    if list_user_input[0].lower() == 'show'and list_user_input[1].lower() == 'all':
+        if dict_phone:
+            for key, value in dict_phone.items():
+                return f'{key = } - {value = }'
+        else:
+             return 'Dict is empty'
+        
+    if list_user_input[0].lower() == 'phone':
+        return print_phone(*list_user_input)
+        
+    if list_user_input[0].lower() == 'help':
+        return helf_cla()
+        
+    if list_user_input[0].lower() == 'change':
+        return change_phon(*list_user_input)
+        
+    if list_user_input[0].lower() == 'close' or list_user_input[0].lower() == 'exit'or (list_user_input[0].lower() == 'good' and list_user_input[1].lower() == 'bye'):
+        return'Good bye!'
+    return 'The command is not correct'    
+
 
 def main():
     print(helf_cla())
     while True:
         stp_user_input= input('<<<< ')
-
-        list_user_input=stp_user_input.split()
-        
-        if list_user_input[0].lower() == 'hello':
-            print('How can I help you?')
-            continue
-        if list_user_input[0].lower() == 'add':
-            print(add_contact(*list_user_input))
-            continue
-        if list_user_input[0].lower() == 'show'and list_user_input[1].lower() == 'all':
-            if dict_phone:
-                for key, value in dict_phone.items():
-                    print(f'{key = } - {value = }')
-            else:
-                print('Dict is empty')
-            continue
-        if list_user_input[0].lower() == 'phone':
-            print(print_phone(*list_user_input))
-            continue
-        if list_user_input[0].lower() == 'help':
-            print(helf_cla())
-            continue
-        if list_user_input[0].lower() == 'change':
-            print(change_phon(*list_user_input))
-            continue
-        if list_user_input[0].lower() == 'close' or list_user_input[0].lower() == 'exit'or (list_user_input[0].lower() == 'good' and list_user_input[1].lower() == 'bye'):
-            print('Good bye!')
+        result_str = command_parser(stp_user_input)
+        print(result_str)
+        if result_str == 'Good bye!':
             break
-        #if list_user_input[0].lower() == 'good' and list_user_input[1].lower() == 'bye'
 
-        # print(list_user_input)
-        # break
+
 
 if __name__ == "__main__":
      main()
